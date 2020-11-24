6daa9afd6aa2241bef5e89eb38006f10c1ebc87d
Daniel Arevalo <danielmateoarevaloangel@gmail.com>
2020-11-24 16:59:17 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tienda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
